bcfce2c17927988a39e585a9aa9cb5d6da43684b
Miao Fan <fanmiao.cslt.thu@gmail.com>
2013-05-05 16:05:35 +0800

    问题 2

--- Problem_2/main.cpp
+++ Problem_2/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <string.h>
+
 using namespace std;
 
 struct Student
@@ -8,30 +9,27 @@ struct Student
 	char name[110];
 	int age;
 	int score;
+} buf[1000];
 
-	bool operator < (const Student &cmp) const
+bool cmp(Student A, Student B)
+{
+	if(A.score != B.score)
+		return A.score < B.score;
+	else
 	{
-		if(score != cmp.score)
-			return score < cmp.score;
+		int tmp = strcmp(A.name, B.name);
+		if(tmp != 0)
+			return tmp < 0;
 		else
-		{
-			int tmp = strcmp(name, cmp.name);
-			if(tmp != 0)
-				return tmp < 0;
-			else
-			{
-				return age < cmp.age;
-			}
-		}
+			return A.age < B.age;
 	}
-} buf[1001];
+};
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
 
 	int n;
-
 	while(scanf("%d", &n) != EOF)
 	{
 		for(int i = 0; i < n; i++)
@@ -39,12 +37,10 @@ int main()
 			scanf("%s%d%d", &buf[i].name, &buf[i].age, &buf[i].score);
 		}
 
-		sort(buf, buf + n);
+		sort(buf, buf + n, cmp);
 
 		for(int i = 0; i < n; i++)
-		{
 			printf("%s %d %d\n", buf[i].name, buf[i].age, buf[i].score);
-		}
 	}
 
 	return 0;
