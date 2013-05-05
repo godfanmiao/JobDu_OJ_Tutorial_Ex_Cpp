52eb322a6676de911b8f806a01cee6118b565ae9
Miao Fan <fanmiao.cslt.thu@gmail.com>
2013-05-05 16:42:31 +0800

    题目 4： EXCEL排序

--- Problem_4/main.cpp
+++ Problem_4/main.cpp
@@ -1,62 +1,67 @@
 #include <stdio.h>
-#include <string.h>
 #include <algorithm>
+#include <string.h>
+
 using namespace std;
 
-int c;
-
+int n, c;
 struct Student
 {
 	char id[10];
-	char name[20];
+	char name[10];
 	int score;
-	
+
 	bool operator < (const Student &cmp) const
 	{
-		int tmp;
+		int t1 = strcmp(id, cmp.id);
+		int t2;
 		switch(c)
 		{
-			case 1:
-				return strcmp(id, cmp.id) < 0;		
-			case 2:
-				tmp = strcmp(name, cmp.name);
-				if(tmp != 0)
-					return tmp < 0;
-				else
-					return strcmp(id, cmp.id) < 0;
-			case 3:
-				if(score != cmp.score)
-					return score < cmp.score;
-				else
-					return strcmp(id, cmp.id) < 0;
+		case 1:
+			return t1 < 0;
+		case 2:
+			t2 = strcmp(name, cmp.name);
+			if(t2 == 0)
+				return t1 < 0;
+			else
+				return t2 < 0;
+		case 3:
+			if(score == cmp.score)
+				return t1 < 0;
+			else
+				return score < cmp.score;
+
 		}
 	}
-} buf[100001];
+} buf[100000];
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
-	
-	int n;
-	int count = 1;
+
+	int cnt = 0;
+
 	while(scanf("%d%d", &n, &c) != EOF)
 	{
 		if(n == 0)
 			break;
+		cnt ++;
+
 		for(int i = 0; i < n; i++)
 		{
 			scanf("%s%s%d", &buf[i].id, &buf[i].name, &buf[i].score);
 		}
-		
+
 		sort(buf, buf + n);
-		
-		printf("Case %d:\n", count++);
+
+		printf("%s %d:\n", "Case", cnt);
 		
 		for(int i = 0; i < n; i++)
 		{
 			printf("%s %s %d\n", buf[i].id, buf[i].name, buf[i].score);
 		}
 	}
-	
+
 	return 0;
 }
+
