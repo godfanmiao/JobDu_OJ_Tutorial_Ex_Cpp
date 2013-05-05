731c7754b112e4a39cdc47242396fe7a247e79bd
Miao Fan <fanmiao.cslt.thu@gmail.com>
2013-05-05 19:55:09 +0800

    题目 10： 统计同成绩学生人数

--- Problem_10/main.cpp
+++ Problem_10/main.cpp
@@ -6,19 +6,19 @@ int main()
 
 	int buf[101];
 
-	int n, score;
-
+	int n;
 	while(scanf("%d", &n) != EOF && n != 0)
 	{
 		for(int i = 0; i <= 100; i++)
 			buf[i] = 0;
 
+		int tmp;
 		for(int i = 0; i < n; i++)
 		{
-			scanf("%d", &score);
-			buf[score] ++;
+			scanf("%d", &tmp);
+			buf[tmp] ++;
 		}
-		
+		int score;
 		scanf("%d", &score);
 		printf("%d\n", buf[score]);
 	}
